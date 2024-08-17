779c1a11d1d3ebd17d340bc015601fc91889e8ec
apple <apple@apples-MacBook-Air.local>
2024-08-17 14:07:15 +0700

    updated: decrypt AES

--- server/api/aes/decrypt.post.ts
+++ server/api/aes/decrypt.post.ts
@@ -5,5 +5,7 @@ export default defineEventHandler(async (event) => {
     const { message, key } = body;
 
   const bytes = CryptoJS.AES.decrypt(message, key);
-  return bytes.toString(CryptoJS.enc.Utf8);
+  const originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+  return originalText;
 });
