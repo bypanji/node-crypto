bb41f9bdfc112bfcee43e11acbc96f8ff93a0988
bypanji <panjiprasena@gmail.com>
2025-05-24 00:39:32 +0700

    added: rsa encryption

---
+++ server/api/rsa/generate.post.ts
@@ -0,0 +1,20 @@
+import crypto from "node:crypto"
+
+export default defineEventHandler(() => {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: "pkcs1",
+            format: "pem"
+        },
+        privateKeyEncoding: {
+            type: "pkcs1",
+            format: "pem"
+        }
+    });
+
+    return {
+        publicKey,
+        privateKey
+    }
+})
